161e44e964da7e67be7764e134d61adfe75d27f2
daveroy086 <51098204+daveroy086@users.noreply.github.com>
2019-05-29 07:18:02 -0400

    Update threepalindromePermutation.js
    added the code i have so far as a comment

--- ArraysAndLists/threepalindromePermutation.js
+++ ArraysAndLists/threepalindromePermutation.js
@@ -14,3 +14,31 @@ function palindromePermutation (aString){
     //after all the doubles are deleted if there aare only zero or one characters left the string was a permutation of a palimdrome
     //check the hints
     }
+
+/*function palindromePermutation (aString){
+    /*Given a string, write a function to check if it is a permutation of a palindrome.
+    *A palindrome is a word or phrase that is the same forwards and backwards.
+    *A permutation is a rearrangement of letters.
+    *The palindrome does not need to be limited to just dictionary words.
+    */
+
+    //turn the string into an array
+    var anArray = aString.split();
+    //delete any empty spaces
+    for ( i = 0 ; i < aString.length; i++) {//this is not working yet
+        console.log(anArray);
+        if (anArray[i] == " ") {
+            anArray.splice(i, 1);
+        }
+    }// fend for i
+    //a for loop to get a letter
+    //another loop to see if it is repeated in the string
+    //if it is repeated delete both
+    //after all the doubles are deleted if there aare only zero or one characters left the string was a permutation of a palimdrome
+    //check the hints
+    }
+
+    palindromePermutation('ab');
+    palindromePermutation('aba');
+    palindromePermutation('abba');
+    palindromePermutation('a b');*/
