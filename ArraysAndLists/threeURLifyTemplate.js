8aa184f85dd09a37f9f7cb9db9a0cc2ecbcb0ac9
daveroy086 <51098204+daveroy086@users.noreply.github.com>
2019-06-04 16:12:13 -0400

    added code

---
+++ ArraysAndLists/threeURLifyTemplate.js
@@ -0,0 +1,51 @@
+function URLify (aString, anInt) {
+    /*Write a method to replace all spaces in a string with '%20'.
+    *Your may assume that the string has sufficient space at the end to hold all the additional characters, 
+    *and that you are given the "true" length of the string.
+    *(Note: If implementing in  Java, please use a character array so that you can perform this operation in place.)
+    *Input:  "Mr John Smith ", 13
+    *Output: "Mr%20John%20Smith"
+    *Hints:#53 and #118
+    *#53. It's often easiest to modify strings by going from the end of the string to the beginning.
+    *#118. You might find you need the number of spaces. Can you just count them?
+    */
+    
+    //make the string an array
+    var anArray = aString.split("");
+    //initialize a string
+    var outputString = "";
+    //make a for loop to go thru all the items in the array
+    for( i = 0; i <= anInt - 1; i++) {
+        if(anArray[i] == " ") {//'if' the char is a space concatenate with '%20'
+            outputString += "%20";
+        }else{//'else' concatenate each item to the string
+            outputString += anArray[i];
+        }console.log(outputString);
+    }//end for
+    //there are two hints but I don't see how they help
+    //I think the hints and the anInt argument must be helpful in other languages
+}
+
+URLify('out put String', 14);
+    *****************
+    /*Write a method to replace all spaces in a string with '%20'.
+    *Your may assume that the string has sufficient space at the end to hold all the additional characters, 
+    *and that you are given the "true" length of the string.
+    *(Note: If implementing in  Java, please use a character array so that you can perform this operation in place.)
+    *Input:  "Mr John Smith ", 13
+    *Output: "Mr%20John%20Smith"
+    *Hints:#53 and #118
+    *#53. It's often easiest to modify strings by going from the end of the string to the beginning.
+    *#118. You might find you need the number of spaces. Can you just count them?
+    */
+    
+    //make the string an array
+    //initialize a string
+    //make a for loop to go thru all the items in the array
+    //'if' the char is a space concatenate with '%20'
+    //'else' concatenate each item to the string
+    
+    //there are two hints but I don't see how they help
+}
+
+URLify('out put String', 14);
